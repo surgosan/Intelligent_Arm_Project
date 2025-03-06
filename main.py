27ad88d7101d4c6a2b2954b20d327de433ed465c
surgosan <surgosan@gmail.com>
2025-03-06 16:16:30 -0500

    arm test VIII

--- main.py
+++ main.py
@@ -55,11 +55,11 @@ def main():
 
     # Open the gripper
     time.sleep(2)
-    node.move_gripper(1.0)  # Adjust based on your gripper limits
+    node.move_gripper(0.2)  # Adjust based on your gripper limits
 
     # Close the gripper
     time.sleep(2)
-    node.move_gripper(-1.0)
+    node.move_gripper(-0.2)
 
     rclpy.shutdown()
 
